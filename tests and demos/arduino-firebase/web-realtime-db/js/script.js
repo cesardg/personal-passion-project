e07b1277ec6ad9a7d0238f938cf4cec3a939d677
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-11-24 13:49:55 +0100

    CRUD firebase realtime database

--- tests and demos/arduino-firebase/web-realtime-db/js/script.js
+++ tests and demos/arduino-firebase/web-realtime-db/js/script.js
@@ -1,52 +1,97 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import {getDatabase, ref, get, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyAdfE9rKt59I0J-qVrvDnOsJAx0-xp1dMI",
-        authDomain: "arduino-wi-fi-xmas-tree-db.firebaseapp.com",
-        databaseURL: "https://arduino-wi-fi-xmas-tree-db-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "arduino-wi-fi-xmas-tree-db",
-        storageBucket: "arduino-wi-fi-xmas-tree-db.appspot.com",
-        messagingSenderId: "838895049591",
-        appId: "1:838895049591:web:6e11c76081c8d1d9f05f72"
-    };
+const firebaseConfig = {
+    apiKey: "AIzaSyAdfE9rKt59I0J-qVrvDnOsJAx0-xp1dMI",
+    authDomain: "arduino-wi-fi-xmas-tree-db.firebaseapp.com",
+    databaseURL: "https://arduino-wi-fi-xmas-tree-db-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "arduino-wi-fi-xmas-tree-db",
+    storageBucket: "arduino-wi-fi-xmas-tree-db.appspot.com",
+    messagingSenderId: "838895049591",
+    appId: "1:838895049591:web:6e11c76081c8d1d9f05f72"
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
-    import {getDatabase, ref, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+let $name= document.querySelector(`.name`);
+let $roll = document.querySelector(`.roll`);
+let $sec = document.querySelector(`.sec`);
+let $gen = document.querySelector(`.gen`);
+const $insBtn = document.querySelector(`.insert`);
+const $selBtn = document.querySelector(`.select`);
+const $updBtn = document.querySelector(`.update`);
+const $delBtn = document.querySelector(`.delete`);
 
-    const db = getDatabase();
+// insert data
+const insertData = (e) => {
+    e.preventDefault();
+    set(ref(db, "TheStudents/"+ $roll.value),{
+        Name: $name.value,
+        Roll: $roll.value,
+        Section: $sec.value,
+        Gender: $gen.value
 
-    const $name= document.querySelector(`.name`);
-    const $roll = document.querySelector(`.roll`);
-    const $sec = document.querySelector(`.sec`);
-    const $gen = document.querySelector(`.gen`);
+    })
+    .then(()=>{
+        console.log("set done")
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
 
-    const $insBtn = document.querySelector(`.insert`);
-    const $selBtn = document.querySelector(`.select`);
-    const $updBtn = document.querySelector(`.update`);
-    const $delBtn = document.querySelector(`.delete`);
+const selectData = (e) => {
+    e.preventDefault();
+    const dbRef = ref(db);
+    get(child(dbRef, "TheStudents/" + $roll.value)).then((snapshot)=>{
+        if (snapshot.exists()){
+            $name.value = snapshot.val().Name;
+            $sec.value = snapshot.val().Section;
+            $gen.value = snapshot.val().Gender;
+        } else{
+            console.log("no data found")
+        }
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
 
-    // insert data
-    const insertData = (e) => {
-        e.preventDefault();
-        set(ref(db, "TheStudents/"+ $roll.value),{
-            Name: $name.value,
-            Roll: $roll.value,
-            Section: $sec.value,
-            Gender: $gen.value
+const updateData = (e) => {
+    e.preventDefault();
+    update(ref(db, "TheStudents/"+ $roll.value),{
+        Name: $name.value,
+        Section: $sec.value,
+        Gender: $gen.value
 
-        })
-        .then(()=>{
-            console.log("set done")
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }
+    })
+    .then(()=>{
+        console.log("update done")
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
 
+const deleteData = (e) => {
+    e.preventDefault();
+    remove(ref(db, "TheStudents/"+ $roll.value))
+    .then(()=>{
+        console.log("remove done")
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+
+
+
+const init = () =>{
     $insBtn.addEventListener('click', insertData);
+    $selBtn.addEventListener('click', selectData);
+    $updBtn.addEventListener('click', updateData);
+    $delBtn.addEventListener('click', deleteData);
+}
+
+init()
