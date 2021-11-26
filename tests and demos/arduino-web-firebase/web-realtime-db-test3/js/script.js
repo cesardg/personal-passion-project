296ff64d3ba0782c8309766b9ee9915d45a5dc45
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-11-26 14:29:51 +0100

    add preview

---
+++ tests and demos/arduino-web-firebase/web-realtime-db-test3/js/script.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import {getDatabase, ref, get, set, child, update, remove} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdfE9rKt59I0J-qVrvDnOsJAx0-xp1dMI",
+    authDomain: "arduino-wi-fi-xmas-tree-db.firebaseapp.com",
+    databaseURL: "https://arduino-wi-fi-xmas-tree-db-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "arduino-wi-fi-xmas-tree-db",
+    storageBucket: "arduino-wi-fi-xmas-tree-db.appspot.com",
+    messagingSenderId: "838895049591",
+    appId: "1:838895049591:web:6e11c76081c8d1d9f05f72"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+const $treeId= document.querySelector(`.tree-id`);
+const $message = document.querySelector(`.message`);
+
+const $sendButton = document.querySelector(`.send`);
+
+const handleClickSend = (e) => {
+    e.preventDefault();
+    update(ref(db, $treeId.value + "/messages"),{
+        message: $message.value  
+    })
+    .then(()=>{
+        console.log("update done")
+        $message.value = "";
+        document.querySelector(`.container`).innerHTML = `<h1 class="title">Your message has been successfully sent!</h1>`
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+
+const addHTMLandCSS = () => {
+    for (let i = 0; i < 300; i++) {
+        const string =  `<div class="led-container led-container-${i}"><p class="led led-${i} ">${i}</p></div>`
+        document.querySelector(`.preview`).innerHTML += string
+    }
+
+
+}
+
+
+const init = () =>{
+    $sendButton.addEventListener('click', handleClickSend);
+    addHTMLandCSS();
+}
+
+init()
